59dcc6ec2af2dd3889ac93d8e5ce27eb18b570e2
Micka Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-06-08 10:37:31 -0300

    Add test registerNewUser class

---
+++ src/test/java/com/alkemy/ong/security/service/userdetailscustomservice/RegisterNewUserShould.java
@@ -0,0 +1,12 @@
+package com.alkemy.ong.security.service.userdetailscustomservice;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegisterNewUserShould {
+
+    @Test
+    void register() {
+    }
+}
