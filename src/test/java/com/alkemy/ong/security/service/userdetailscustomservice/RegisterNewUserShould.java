ca0fe7f986a3662a3668184f4bd3242b8edb8c41
Micka Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-06-08 12:31:23 -0300

    Test to verify that an exception is thrown when the user is already registered

--- src/test/java/com/alkemy/ong/security/service/userdetailscustomservice/RegisterNewUserShould.java
+++ src/test/java/com/alkemy/ong/security/service/userdetailscustomservice/RegisterNewUserShould.java
@@ -1,12 +1,27 @@
 package com.alkemy.ong.security.service.userdetailscustomservice;
 
+import com.alkemy.ong.controller.utils.AuthBasic;
+import com.alkemy.ong.exception.DataAlreadyExistException;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
+class RegisterNewUserShould extends AuthBasic {
 
-class RegisterNewUserShould {
-
-    @Test
-    void register() {
+    @Test()
+    void throw_exception_when_email_already_exist() throws Exception {
+        request.setEmail("leonardo@gmail.com");
+        Mockito.when(userAuthService.register(request)).thenThrow(new DataAlreadyExistException("The user already esist in our Data Base."));
     }
+
+
 }
