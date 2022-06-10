3faf3a0457ff10b22d92b944eb263112724af26f
Micka Tarazaga <94411297+mickaelatarazaga@users.noreply.github.com>
2022-06-10 12:09:30 -0300

    Add mother class with the elements in common to make the code cleaner

--- src/test/java/com/alkemy/ong/controller/utils/RegisterBasic.java
+++ src/test/java/com/alkemy/ong/controller/utils/RegisterBasic.java
@@ -1,31 +1,34 @@
 package com.alkemy.ong.controller.utils;
 
 
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import org.junit.jupiter.api.BeforeEach;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.alkemy.ong.model.Role;
-import com.alkemy.ong.security.model.UserEntity;
+import com.alkemy.ong.repository.UserRepository;
 import com.alkemy.ong.security.dto.UserLoginRequest;
 import com.alkemy.ong.security.dto.UserRegisterRequest;
 import com.alkemy.ong.security.dto.UserRegisterResponse;
 import com.alkemy.ong.security.enums.RoleEnum;
+import com.alkemy.ong.security.mapper.UserMapper;
+import com.alkemy.ong.security.model.UserEntity;
+import com.alkemy.ong.security.service.JwtUtils;
 import com.alkemy.ong.security.service.UserDetailsCustomService;
 import com.alkemy.ong.service.RoleService;
-import java.util.ArrayList;
-import java.util.List;
-import com.alkemy.ong.repository.UserRepository;
-import com.alkemy.ong.security.service.JwtUtils;
 import com.alkemy.ong.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-public class AuthBasic {
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+public class RegisterBasic {
    
     @MockBean
     protected UserDetailsCustomService userAuthService;
@@ -45,12 +48,14 @@ public class AuthBasic {
     protected MockMvc mockMvc;
     @Autowired
     protected ObjectMapper objectMapper;
+    @Autowired
+    protected UserMapper userMapper;
     @MockBean
     protected UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken;
     protected UserEntity user;
+    protected UserEntity userRegister;
     protected UserRegisterResponse response;
     protected UserRegisterRequest request;
-    protected UserLoginRequest loginRequest;
     protected String jwt;
 
     @BeforeEach
@@ -59,10 +64,6 @@ public class AuthBasic {
         //ROLES CONFIG
         List<Role> roleList = new ArrayList<>();
         roleList.add(roleService.findByName(RoleEnum.USER.getRoleName()));
-        //LOGIN REQUEST
-        loginRequest = new UserLoginRequest();
-        loginRequest.setEmail("tarazaga.mickaela@gmail.com");
-        loginRequest.setPassword("password123");
         //USER ENTITY
         user = new UserEntity();
         user.setId(1L);
@@ -87,5 +88,14 @@ public class AuthBasic {
         request.setLastName("Tarazaga");
         request.setPassword("password123");
         request.setPhoto(null);
+        //USER ENTITY
+        userRegister = new UserEntity();
+        userRegister.setId(1L);
+        userRegister.setEmail("tarazaga.mickaela@gmail.com");
+        userRegister.setFirstName("Mickaela");
+        userRegister.setLastName("Tarazaga");
+        userRegister.setPassword("password123");
+        userRegister.setPhoto(null);
+        userRegister.setRoles(roleList);
     }
 }
